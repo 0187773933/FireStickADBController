ccd8662a92206dc3f5a586e60606ec7b28774c61
0187773933 <collincerbus@student.olympic.edu>
2021-08-05 06:32:04 -0400

    6

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/0187773933/FireStickADBController
 go 1.16
 
 require (
-	github.com/0187773933/ADBWrapper v0.0.0-20210805091501-3a976f9faa98
+	github.com/0187773933/ADBWrapper v0.0.0-20210805103053-5c40bde0ab70
 	github.com/manucorporat/try v0.0.0-20170609134256-2a0c6b941d52 // indirect
 )
--- updateGoModule.sh
+++ updateGoModule.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 UsernameModuleName="0187773933/ADBWrapper"
-GitHash=$(curl -s 'https://api.github.com/repos/$UsernameModuleName/git/refs/heads/master' | jq -r '.object.sha')
+GitHash=$(curl -s https://api.github.com/repos/$UsernameModuleName/git/refs/heads/master | jq -r '.object.sha')
 go get -u "github.com/$UsernameModuleName/@$GitHash"
